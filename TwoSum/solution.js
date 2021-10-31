b03f8d65e7785cef2edffdb06d016d738650c329
Nadwine <nadwine@louisnana@outlook.com>
2021-10-31 14:49:17 +0000

    solution solved

---
+++ TwoSum/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+    const previousValues = {}
+    for (let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i]
+        const neededValues = target - currentNumber
+        const index2 = previousValues[neededValues]
+        if (index2 != null){
+            return [index2, i]
+        } else{
+            previousValues[currentNumber] = i
+
+        }
+    }
+
+};
+
