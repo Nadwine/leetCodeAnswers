2f75a6b8f3543ca6a9d56fad6bc0008123e46932
Nadwine <nadwine@louisnana@outlook.com>
2021-10-31 15:30:10 +0000

    solution solved

---
+++ LongestCommonPrefix/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+     // prefix = ''
+     let prefix = ''
+     if (strs.length === 0) return prefix
+     // loop through characters
+     for (let i = 0; i < strs[0].length; i++){
+         const character = strs[0][i]
+         for (let j = 0; j < strs.length; j++){
+             if (strs[j][i] !== character) return prefix
+         }
+         prefix = prefix + character
+     }
+     return prefix
+
+    
+};
