0f42e7612d9c1f7a70a90213faa748697c7c572e
Nadwine <louisnana@outlook.com>
2021-11-01 19:57:44 +0000

    merge

--- Remove_One_Element_To_Make_The_Array_Strictly_Increasing/solution.js
+++ Remove_One_Element_To_Make_The_Array_Strictly_Increasing/solution.js
@@ -2,22 +2,24 @@
  * @param {number[]} nums
  * @return {boolean}
  */
- var canBeIncreasing = function(nums) {
-
-     for (let i = 0; i < nums.length; i++) { 
-        if (nums[i] > nums[i+1]){
-            nums.splice(i,1)
-            return true
+var canBeIncreasing = function (nums) {
+    let isRemoved = false
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] <= nums[i-1]){
+                if (isRemoved) {
+                    return false;
         }
+        else isRemoved = true;
+        if (i > 1 && nums[i] <= nums[i-2]){
+            nums[i] = nums[i-1];
+        }
+               
+        }  
     }
-     for (let i = 0; i < nums.length; i++){
-         if (!nums[i] > nums[i+1]){
-             return false
-         }
-     }
-    
+        
+    return true
+     
 };
 
-console.log(canBeIncreasing ([1,2,10,5,7]))
 
-   
+
