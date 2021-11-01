a81cf53e5a6fd9fdd3032c2e20380a1b8b344387
Nadwine <louisnana@outlook.com>
2021-11-01 19:59:08 +0000

    solution solved

--- Remove_One_Element_To_Make_The_Array_Strictly_Increasing/solution.js
+++ Remove_One_Element_To_Make_The_Array_Strictly_Increasing/solution.js
@@ -4,21 +4,21 @@
  */
 var canBeIncreasing = function (nums) {
     let isRemoved = false
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] <= nums[i-1]){
-                if (isRemoved) {
-                    return false;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            if (isRemoved) {
+                return false;
+            }
+            else isRemoved = true;
+            if (i > 1 && nums[i] <= nums[i - 2]) {
+                nums[i] = nums[i - 1];
+            }
+
         }
-        else isRemoved = true;
-        if (i > 1 && nums[i] <= nums[i-2]){
-            nums[i] = nums[i-1];
-        }
-               
-        }  
     }
-        
+
     return true
-     
+
 };
 
 
